106619c9e4d5fe9c3e1cd20cd094ce45dac4a945
Jeffrey Baldwin <jbaldwin@youngliving.com>
2021-12-17 16:23:06 -0700

    add my stuff

---
+++ reporting/five9.ps1
@@ -0,0 +1,35 @@
+# Constants
+$AWSProfile = "five9prod"
+$s3Bucket = "647117382455-five9-snowflake"
+# List of Reports
+#$reports = "Agent Disposition Report (Global)", "Agent Information (Global)", "Agent Login-Logout (Global)", "Agent Ready and Call Data by Day (Global)", "Agent States by Day (Global)", "Call Log - Queue Call Back Offered Registered = 1", "Call Log - Queue Call Back Processing = 0", "Call Log - Queue Call Back Processing = 1", "Call Log - Queue Call Back Registered = 0", "Missed Calls - All Teams", "Ready States by Agent by Interval (Global)", "Reason Codes by Interval (Global)", "Survey Report (Global)", "Updated Dispositions Reporting/Forecasting", "Queue Quality of Service by Interval,Voicemails Export"
+for ( $reportNum = 0; $reportNum -lt 13; $reportNum++ ) {
+switch ( $reportNum )
+{
+    0 { $prefix = 'AgentDisp'     }
+    1 { $prefix = 'AgentInfo'     }
+    2 { $prefix = 'AgentLogin'    }
+    3 { $prefix = 'AgentReady'    }
+    4 { $prefix = 'AgentStates'   }
+    5 { $prefix = 'CallLog1'      }
+    6 { $prefix = 'CallLog2'      }
+    7 { $prefix = 'CallLog3'      }
+    8 { $prefix = 'CallLog4'      }
+    9 { $prefix = 'MissedCalls'   }
+    10 { $prefix = 'ReadyStates'  }
+    11 { $prefix = 'ReasonCodes'  }
+    12 { $prefix = 'SurveyReport' }
+    13 { $prefix = 'UpdatedDisp'  }  #Broken
+    14 { $prefix = 'Voicemails'   }  #Broken
+}
+$filedate =  get-date -Format filedatetime
+$infile = "E:\git\Five9-API\reporting\Output\$prefix.csv"
+$outfile = "E:\git\Five9-API\reporting\Output\$prefix$filedate.json"
+Set-Location "e:\git\five9-api\reporting"
+python ".\$prefix.py"
+#Write-host $reportNum $infile $prefix$filedate
+import-csv $infile | ConvertTo-Json | Add-Content -Path $outfile -Force
+
+# Upload file to S3
+Write-S3Object -ProfileName $AWSprofile -BucketName $s3Bucket -Key "five9/$prefix/$prefix$filedate.json" -File $outfile
+}
--- reporting/runReport.py
+++ reporting/runReport.py
@@ -1,6 +1,10 @@
 import csv
+import json
 import datetime
 import time
+from io import StringIO
+
+from requests.sessions import dispatch_hook
 
 from lxml import etree
 import requests
@@ -14,6 +18,7 @@ try:
 except:
     print('No saved account credentials discovered')
     ACCOUNTS = {}
+
 
 # This function prints the SOAP envelope for the request and/or response
 def zeep_history_print(history, last_sent=True, last_received=True):
@@ -72,92 +77,34 @@ except requests.exceptions.HTTPError as e:
 # the "Custom Reports" section in Five9 reporting portal
 criteria_datetime_start = datetime.date.today()
 criteria_datetime_end = datetime.date.today() + datetime.timedelta(days=1)
-report_folder = "staging"
-report_name = "staging_callLog"
+report_folder = "ETL"
+report_name = "Agent Disposition Report (Global)"
 
 # Criteria object contains required parameters forthe runReport method
 report_criteria = {
     "time": {
         "start": criteria_datetime_start.strftime("%Y-%m-%dT%H:%M:%S.000"),
         "end": criteria_datetime_end.strftime("%Y-%m-%dT%H:%M:%S.000"),
-    },
-    # Optionally filter the report by an additional objectType <work in progress>
-    # This is generally best done in the report definition in the Five9 portal. 
-    # In the below example, the report could be filtered for records where
-    # the skill is equal to sample skills "omni" or "outreach_en"
-    # 
-    # "reportObjects": {
-    #     "objectNames": [
-    #         "omni", 
-    #         "outreach_en",
-    #     ],
-    #     "objectType": "Skill",
-    # }
-
-    # reportObjects can by of objectType
-    #       AgentGroup
-    #       Campaign
-    #       CampaignProfile
-    #       CrmField
-    #       Disposition
-    #       List
-    #       Prompt
-    #       ReasonCode
-    #       Skill
-    #       User
-    #       UserProfile
-    #       IvrScript
-    #       CallVariableGroup
-    #       CallVariable
-    #       Connector
+    }
 }
-
-
-
 # Request the report to be generated.  Will respond with a report run ID
 try:
     report_run_id = client.service.runReport(
         folderName=report_folder,
         reportName=report_name,
         criteria=report_criteria)
-    print(f'Report Id Requested: {report_run_id}')
+    #print(f'Report Id Requested: {report_run_id}')
 except zeep.exceptions.Fault as e:
     print(e)
-
-
-
-
-# Sample soap envelope payloads for the runReport request and response
-print('\nSOAP envelopes for request and response:')
-zeep_history_print(history)
-
-# Check to see if the report has finished running.
-# IMPORTANT: failure to pause for some time between each check to see if the 
 # report has run can cause you to exceed your API limits.
 report_running = True
 checks = 0
 while report_running is True:
     report_running = client.service.isReportRunning(report_run_id, timeout=10)
     if report_running is True:
-        print(f"Still Running ({checks})", end="\r")
-        # IMPORTANT - invoking this method on a loop for a long-running report 
-        # WILL burn up your API rate limits, put some time between requests
         time.sleep(5)
-
-print("DONE Running")
-
 # Csv result set is faster to obtain, less verbose response body
 reportResultCsv = client.service.getReportResultCsv(report_run_id)
-row_count_in_result_csv = len(reportResultCsv)-1
-print(reportResultCsv)
-
-# This is more convenient if you don't want to parse the csv data
-reportResult = client.service.getReportResult(report_run_id)
-
-row_count_in_result = len(reportResult.records)
-print(f'Row count: {row_count_in_result}\n')
-
-# print(reportResult)
-
-# Insert the data into your database 
-# or write to a file for another process to consume
+csvFilePath = 'E:\git\Five9-API\reporting\AgentDisp.csv'
+with open(csvFilePath, 'w', encoding='utf-8') as csvf:
+    csvf.write(reportResultCsv)
